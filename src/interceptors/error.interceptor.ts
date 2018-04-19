c1d6f1faf5e25db71ceb368b582a31ca5a3ad01d
Alessandro Duarte Junior <alessandroduartejunior@gmail.com>
2018-04-19 10:47:16 -0300

    Interceptando 403 e redirecionando

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -1,7 +1,12 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).catch((error, cought) => {
@@ -17,8 +22,18 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             console.log(errorObj);
 
+            switch (errorObj.status) {
+                case 403:
+                    this.handle403();
+                    break;
+            }
+
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    private handle403() {
+        this.storage.setUserLocal(null);
     }
 }
 
