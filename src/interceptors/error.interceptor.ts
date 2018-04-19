fccb2330ad68bcd550ecd396f09b76450bfb1aff
Alessandro Duarte Junior <alessandroduartejunior@gmail.com>
2018-04-19 11:59:05 -0300

    Tratamento padrao para outros erros

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -2,11 +2,12 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) { }
+    constructor(public storage: StorageService, public alertController: AlertController) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).catch((error, cought) => {
@@ -23,9 +24,17 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch (errorObj.status) {
+
                 case 403:
                     this.handle403();
                     break;
+
+                case 401:
+                    this.handle401();
+                    break;
+
+                default:
+                    this.handleDefault(errorObj);
             }
 
             return Observable.throw(errorObj);
@@ -34,6 +43,26 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     private handle403() {
         this.storage.setUserLocal(null);
+    }
+
+    private handle401() {
+        let alert = this.alertController.create({
+            title: 'Erro 401: Falha de autenticação!',
+            message: 'Email ou senha inválidos',
+            enableBackdropDismiss: false,
+            buttons: [{ text: 'OK' }]
+        });
+        alert.present();
+    }
+
+    private handleDefault(objError) {
+        let alert = this.alertController.create({
+            title: 'Erro ' + objError.status + ':' + objError.error,
+            message: objError.message,
+            enableBackdropDismiss: false,
+            buttons: [{ text: 'OK' }]
+        });
+        alert.present();
     }
 }
 
