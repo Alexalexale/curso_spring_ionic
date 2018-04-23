cc89e20cc303aef8cd28ca6d882d802ad46375e7
Alessandro Duarte Junior <alessandroduartejunior@gmail.com>
2018-04-23 11:04:19 -0300

    tirando foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { PREFIX_CONSTANT } from '../../enums/prefix.enum';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -14,8 +15,11 @@ import { PREFIX_CONSTANT } from '../../enums/prefix.enum';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService,
+    public camera: Camera) { }
 
   ionViewDidLoad() {
     let localUser = this.storage.getUserLocal();
@@ -37,6 +41,23 @@ export class ProfilePage {
     this.clienteService.getImagemFromBucket(this.cliente.id).subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/${PREFIX_CONSTANT.CLIENTE + this.cliente.id}.jpg`;
     }, erros => { });
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => { });
   }
 
 }
